54c4bc56e02cf83aadea241abf89005bbfc7960a
Nils Martel <nilsmartel@yahoo.de>
2026-08-14 12:36:19 +0200

    Write out error function

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,24 @@ struct Model {
     edges: Vec<(usize, usize)>,
 }
 
+fn error(model: &Model) -> f32 {
+    const DESIRED_DISTANCE: f32 = 1.0;
+
+    let mut sum = 0.0;
+    for (i, j) in &model.edges {
+        let v1: Vec2 = model.verts[*i];
+        let v2: Vec2 = model.verts[*j];
+
+        // compute eucl. distance
+        let v = v1 - v2;
+        let distance = v.dot(v).sqrt();
+
+        // we add the power, so that no harsh outliers are tolerated by the computation algorithm
+        // and the error is always >= 0
+        let error = (distance - DESIRED_DISTANCE).powi(2);
+
+        sum += error;
+    }
+
+    sum
+}
